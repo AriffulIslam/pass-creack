93f9626e6b9ff8ff58ca29e52c9e6f145f01c28b
AriffulIslam <78169786+AriffulIslam@users.noreply.github.com>
2021-07-27 12:05:53 +0600

    Add files via upload

--- hacker.py
+++ hacker.py
@@ -17,7 +17,7 @@ yellow = '\n\n\x1b[1;93m'
 os.system('clear')
 fuck = red + " \xe2\x9a\xa0\xef\xb8\x8f Warning : We are not responsible if someone misuses it."
 notice = yellow + '                \xe2\x9c\xaaFacebook Account Hacking Tools\xe2\x9c\xaa                      '
-logop ( ' \n \x1b[99m    888888           888888      	   888            888 \n 888      888       888      88       888            888 \n 888      888       888      88       888            888 \n 888888888        888888            888            888 \n 888      888       888   888         888            888 \n 888      888       888      888        88888888888 \n 888      888       888         888       .88888888 \n ')
+logop (green + ' \x1b[99m\n    888888           888888      	   888            888 \n 888      888       888      88       888            888 \n 888      888       888      88       888            888 \n 888888888        888888            888            888 \n 888      888       888   888         888            888 \n 888      888       888      888        88888888888 \n 888      888       888         888       .88888888 \n ')
 print ''
 print ''
 print ' Noakhailla Mal.We Can Make System, We Can Fuck System '

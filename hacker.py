586f141eb6ee7a1cad087a5ddfa887e390cb3024
AriffulIslam <78169786+AriffulIslam@users.noreply.github.com>
2021-07-27 08:36:05 +0600

    Add files via upload

--- hacker.py
+++ hacker.py
@@ -52,8 +52,7 @@ print ''
 print ''
 from tqdm import tqdm
 loop = tqdm(total = 5000, position=0, leave=False)
-for k in range(5000) :
-loop.set_description("ckeacking user info".format(k))
+for k in range(5000) : loop.set_description("ckeacking user info".format(k))
 loop.update(1)
 lop.close()
 logop (green + ' Creacking User Info.')
